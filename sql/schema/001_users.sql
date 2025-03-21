0ffa018b0b37de6bf93af7ee71f4eee3d0f8b45e
adjaydev <contact@adjay.dev>
2025-03-21 13:49:48 +0100

    update

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE users (
+	id UUID PRIMARY KEY,
+	created_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP NOT NULL,
+	name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
+
+-- postgres://localhost/gator
+-- goose postgres "postgres://localhost/gator" up
